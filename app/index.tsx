291d78f7b854dc9536385ac3ac6a11a6084fad24
saoko1x <jemg2510@gmail.com>
2024-01-06 16:42:20 -0600

    Working with Nativewind and Typescript with Expo Router

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text, TouchableOpacity } from "react-native";
+
+export default function HomePage() {
+  return (
+    <View className="flex-1 justify-center items-center bg-gray-200">
+      <Text className="text-2xl font-bold">Home</Text>
+    </View>
+  );
+}
